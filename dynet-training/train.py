0838f32e1fbd83d87faf21d0e6eb73a361bc6b70
ranapop <roxanappop7@gmail.com>
2021-01-30 14:05:28 +0200

    add evaluation flow

--- dynet-training/train.py
+++ dynet-training/train.py
@@ -4,7 +4,7 @@ from input_pipeline.data_extraction import load_from_file, DataSet
 from input_pipeline.oracle import generate_sequence_of_actions, apply_action
 
 NO_EPOCHS = 20
-EMBEDDINGS_SIZE = 20
+EMBEDDINGS_SIZE = 50
 LSTM_NUM_OF_LAYERS = 1
 LSTM_STATE_SIZE = 25
 BILSTM_STATE_SIZE = 2 * LSTM_STATE_SIZE
@@ -15,6 +15,7 @@ NO_OUTPUT_CLASSES = 3
 
 UD_ENGLISH_PATH = 'data/ud-treebanks-v2.7/UD_English-'
 ENGLISH_PARTUT = 'ParTUT/en_partut-ud-{0}.conllu'
+ENGLISH_EWT = 'EWT/en_ewt-ud-{0}.conllu'
 
 class ArcStandardModel():
 
@@ -27,7 +28,7 @@ class ArcStandardModel():
   def create_cg(self, sentence: List[int], action_types: List[int], train: bool):
     # Build new computational graph.
     dy.renew_cg()
-    bilstm_repr = self.sentence_encoder.add_to_cg(sentence)
+    bilstm_repr = self.sentence_encoder.add_to_cg(sentence, train)
     n = len(sentence)
     stack = []
     buffer = list(range(n))
@@ -40,9 +41,14 @@ class ArcStandardModel():
       probs = dy.softmax(logits)
       loss = -dy.log(dy.pick(probs, gold_action_type.value))
       losses.append(loss)
-      predicted_actions.append(probs.npvalue().argmax())
+      predicted_action = probs.npvalue().argmax()
+      predicted_actions.append(predicted_action)
       #TODO: Arc history is not needed at this point
-      apply_action(gold_action_type, stack, buffer, arc_history)
+      if train:
+        apply_action(gold_action_type, stack, buffer, arc_history)
+      else:
+        # TODO: count incomplete parses / do head accuracy.
+        apply_action(predicted_action, stack, buffer, arc_history)
     loss = dy.esum(losses)
     loss_value = loss.value()
     gold_actions = [action_type.value for action_type in action_types]
@@ -71,13 +77,16 @@ class SentenceEncoder():
     self.enc_bwd_lstm = dy.LSTMBuilder(
       LSTM_NUM_OF_LAYERS, EMBEDDINGS_SIZE, LSTM_STATE_SIZE, model)
     
-  def add_to_cg(self, tokens):
+  def add_to_cg(self, tokens, train=False):
     """
     Returns:
       a list of BiLSTM representation (a representation for each token in the
       sentence). Each element is a vector of size BILSTM_STATE_SIZE
     """
     embeddings = [self.input_lookup[token] for token in tokens]
+    if train:
+      self.enc_fwd_lstm.set_dropouts(0.2, 0.2)
+      self.enc_bwd_lstm.set_dropouts(0.2, 0.2)
     fwd_init_state = self.enc_fwd_lstm.initial_state()
     bwd_init_state = self.enc_bwd_lstm.initial_state()
     fw_exps = fwd_init_state.transduce(embeddings)
@@ -132,9 +141,6 @@ def train_one_epoch(arc_standard_model: ArcStandardModel, dataset: DataSet):
   valid_entries = 0
   i = 0
   for data_entry in dataset.dataset_entries:
-    # if i>2:
-    #   break
-    # i+=1
     act_seq = generate_sequence_of_actions(data_entry.tokens,
                                            data_entry.heads,
                                            data_entry.labels)
@@ -151,28 +157,21 @@ def train_one_epoch(arc_standard_model: ArcStandardModel, dataset: DataSet):
   avg_accuracy = sum_accuracy/valid_entries
   return avg_loss, avg_accuracy
 
-def train_model(train_data: DataSet):
-  vocab_size = len(train_data.tokens_vocab.keys())
-  arc_standard_model = ArcStandardModel(vocab_size)
-  for epoch in range(NO_EPOCHS):
-    avg_loss, avg_accuracy = train_one_epoch(arc_standard_model, train_data)
-    print('Epoch {0} Loss {1} Accuracy {2}'.format(epoch, avg_loss, avg_accuracy))
-  return arc_standard_model
-
-def evaluate_model(eval_data: DataSet, trained_model: ArcStandardModel):
+def evaluate_model(trained_model: ArcStandardModel, eval_data: DataSet):
   sum_loss = 0
   sum_accuracy = 0
   valid_entries = 0
-  for data_entry in dataset.dataset_entries:
+  for data_entry in eval_data.dataset_entries:
     act_seq = generate_sequence_of_actions(data_entry.tokens,
                                            data_entry.heads,
                                            data_entry.labels)
     # Should we only test on valid entries??
     if act_seq:
       act_types = act_seq[0]
-      predicted, loss_value, accuracy = arc_standard_model.create_cg(data_entry.tokens,
-                                                       act_types,
-                                                       train=False)
+      predicted, loss_value, accuracy = trained_model.create_cg(
+        data_entry.tokens,
+        act_types,
+        train=False)
       sum_loss += loss_value
       sum_accuracy += accuracy
       valid_entries += 1
@@ -180,8 +179,28 @@ def evaluate_model(eval_data: DataSet, trained_model: ArcStandardModel):
   avg_accuracy = sum_accuracy/valid_entries
   return avg_loss, avg_accuracy
 
+def train_model(train_data: DataSet, dev_data: DataSet):
+  vocab_size = len(train_data.tokens_vocab.keys())
+  arc_standard_model = ArcStandardModel(vocab_size)
+  for epoch in range(NO_EPOCHS):
+    train_loss, train_acc = train_one_epoch(arc_standard_model, train_data)
+    dev_loss, dev_acc = evaluate_model(arc_standard_model, dev_data)
+    print('Epoch {0} Train loss {1}, acc {2}; Dev loss {3}, acc {4}'.format(
+      epoch, train_loss, train_acc, dev_loss, dev_acc))
+  return arc_standard_model
+
+def get_data_paths(dataset_id: str):
+  if dataset_id == 'ParTUT':
+    train_path = UD_ENGLISH_PATH + ENGLISH_PARTUT.format('train')
+    dev_path = UD_ENGLISH_PATH + ENGLISH_PARTUT.format('dev')
+  elif dataset_id == 'EWT':
+    train_path = UD_ENGLISH_PATH + ENGLISH_EWT.format('train')
+    dev_path = UD_ENGLISH_PATH + ENGLISH_EWT.format('dev')
+  return train_path, dev_path
+    
 
 if __name__ == '__main__':
-  train_path = UD_ENGLISH_PATH + ENGLISH_PARTUT.format('train')
+  train_path, dev_path = get_data_paths('EWT')
   train_data = load_from_file(train_path)
-  arc_standard_model = train_model(train_data)
+  dev_data = load_from_file(dev_path)
+  arc_standard_model = train_model(train_data, dev_data)
